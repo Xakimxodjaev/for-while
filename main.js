553d94537bc42aa5f8110addaf387cc650cd61cf
Xakimxodjaev <f53660668@gmail.com>
2025-08-15 15:21:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+// console.log("for boshlandi");
+// for (let i = 0; i < 101; i++) {
+//     console.log(i);
+// }
+// console.log("for tugadi");
+
+
+
+// let arr = ["olma", "behi", "banan", "awp", "desert eagle"]
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+
+
+
+// const numsArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// newarr = []
+
+// for (let i = 0; i < numsArray.length; i++) {
+//     newarr.push(numsArray[i] **2);
+    
+// }
+
+// console.log(newarr);
+ 
+
+
+let numbers = [2, 4, 9, 10, 45, 67, 8, 90];
+console.log(numbers, "boshlanishdagi array");
+
+let evenNumbers = [];
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        evenNumbers.push(numbers[i]);
+    }
+}
+
+console.log(evenNumbers, "juft sonlarga ajratilgan array");
+
+
